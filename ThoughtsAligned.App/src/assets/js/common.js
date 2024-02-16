5f7cbfc15d57c776069c62d39f273084c6068b27
sukhdev <sukh.chd.it@gmail.com>
2024-02-16 11:58:14 +0530

    latest updated

--- ThoughtsAligned.App/src/assets/js/common.js
+++ ThoughtsAligned.App/src/assets/js/common.js
@@ -2,9 +2,23 @@ $(document).ready(function () {
     //Navigation
     $('header .menu').click(function () {
         $('header nav').addClass('activeNav');
+        $('body').addClass('noScroll');
     });
 
     $('header nav .closeNav').click(function () {
         $('header nav').removeClass('activeNav');
+        $('body').removeClass('noScroll');
     });
 });
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 500) {
+        $("header").addClass("darkHeader");
+        $("footer").addClass("darkFooter");
+    } else {
+        $("header").removeClass("darkHeader");
+        $("footer").removeClass("darkFooter");
+    }
+}); 
